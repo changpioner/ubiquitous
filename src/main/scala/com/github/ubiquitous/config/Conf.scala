d37776eb0bd4c0eec0dd989e2aada6f1ad059d43
zhaite <zhaite@jiandan100.cn>
2020-05-11 18:56:51 +0800

    合并trigger线程

---
+++ src/main/scala/com/github/ubiquitous/config/Conf.scala
@@ -0,0 +1,36 @@
+package com.github.ubiquitous.config
+
+import java.util.concurrent.TimeUnit
+
+import com.typesafe.config.{Config, ConfigFactory}
+
+/**
+  *
+  * @author Namhwik on 2020-05-11 15:24
+  */
+object Conf {
+
+  private val config: Config = ConfigFactory.load("properties")
+
+
+  final val TIME_UNIT = config.getString("unit.granularity") match {
+    case "seconds" => TimeUnit.SECONDS
+    case "minutes" => TimeUnit.MINUTES
+    case "hours" => TimeUnit.HOURS
+    case _ => null
+  }
+
+  assertSettings()
+
+
+  def getString(k: String): String =
+    config.getString(s"unit.$k")
+
+
+  def assertSettings(): Unit = {
+    require(TIME_UNIT != null,
+      s"Time granularity does not support : ${config.getString("unit.granularity")}")
+  }
+
+
+}
