49554fae465559260d03e77a465f42a84154094f
zhaite <zhaite@jiandan100.cn>
2020-06-16 10:46:39 +0800

    add exception handle

--- spark-sdq/src/main/scala/com/github/ubiquitous/spark/kafka/DelayTask.scala
+++ spark-sdq/src/main/scala/com/github/ubiquitous/spark/kafka/DelayTask.scala
@@ -4,6 +4,7 @@ import com.github.ubiquitous.config.Conf.config
 import com.github.ubiquitous.utils.HbaseUtil
 import com.github.ubiquitous.wheel.{Task, WheelFactory}
 import org.apache.kafka.clients.producer.{Producer, ProducerRecord}
+import org.apache.log4j.Logger
 
 /**
   *
@@ -12,23 +13,39 @@ import org.apache.kafka.clients.producer.{Producer, ProducerRecord}
 abstract class DelayTask[K, V](dl: Int, k: K, var msg: V)
   extends Task[Unit](dl: Int) {
 
+  private val logger: Logger = Logger.getLogger(this.getClass)
+
   private final val KEY_CACHE_TABLE = config.getString("cache.table")
   private final val KEY_CACHE_FAMILY = config.getString("cache.family")
-
+  private final val KEY_CACHE_DELAY = config.getString("cache.delay")
   private val PERSIST_V: Boolean = config.getBoolean("cache.ifPersistValue")
 
   private final val CH_COL_META = "meta"
   private final val CH_COL_TIME = "time"
 
 
-  if (PERSIST_V) persistKey(cacheKey, msg)
+  override def persist(): Boolean = {
+    !PERSIST_V || {
+      try {
+        HbaseUtil.insertV2(KEY_CACHE_TABLE, k, KEY_CACHE_FAMILY,
+          Map(
+            CH_COL_TIME -> System.currentTimeMillis(),
+            KEY_CACHE_DELAY -> dl,
+            CH_COL_META -> msg
+          )
+        )
+        true
+      } catch {
+        case ex: Exception =>
+          // log the exception msg
+          logger.error(s" error occurred when persist delay task , msg : ${ex.getMessage}")
+          false
+      }
+    }
+  }
 
   def persistKey[KT, VT](key: KT, v: VT): Unit = {
-    HbaseUtil.insertV2(KEY_CACHE_TABLE, key, KEY_CACHE_FAMILY,
-      Map(CH_COL_TIME -> System.currentTimeMillis(),
-        CH_COL_META -> v
-      )
-    )
+
     msg = null.asInstanceOf[V]
   }
 
--- spark-sdq/src/main/scala/com/github/ubiquitous/spark/kafka/TaskMeta.scala
+++ spark-sdq/src/main/scala/com/github/ubiquitous/spark/kafka/TaskMeta.scala
@@ -1,10 +1,9 @@
 package com.github.ubiquitous.spark.kafka
-package com.github.ubiquitous.spark.kafka
 
 /**
   *
   * @author Namhwik on 2020-06-05 15:34
   */
-class TaskMeta {
+case class TaskMeta(key: Any, value: Any, delay: Int) {
 
 }
